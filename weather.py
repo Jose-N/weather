aab726e56bfe811d27d715567346c2ee45ab4800
Jose-N <jose.c.naylor@gmail.com>
2015-10-26 07:03:15 -0400

    Added ability to get the current temperature

--- weather.py
+++ weather.py
@@ -1,18 +1,25 @@
 #! python3
 # weather.py
-# Scrapes http://www.wunderground.com/ to grab weather information that can be called from command line
+# Scrapes http://www.accuweather.com/ to grab weather information that can be called from command line
 
-import sys,request
-for bs4 import BeautifulSoup
+import sys,requests
+from bs4 import BeautifulSoup
 
 # Activate from command line
 if len(sys.argv) > 1:
 	# Use if statments to determine what to do
 	# Get current weather
-	if "today" == ''.join(sys.argv[1]):
+	if "current" == ''.join(sys.argv[1]):
+		todayRes = requests.get("http://www.accuweather.com/en/us/philadelphia-pa/19107/weather-forecast/350540")
+		todayRes.raise_for_status()
+		todaySoup = BeautifulSoup(todayRes.text, "html.parser")
+		todayWeather = todaySoup.select("#feed-main .temp")
+		currentWeather = todayWeather[0].getText()
+		print("It's " + currentWeather + "F right now.")
+	# Get todays weather
 	# Get hourly weather
-	if "hourly" == ''.join(sys.argv[1]):
+	#if "hourly" == ''.join(sys.argv[1]):
 	# 10 day weather forecast
-	if "10day" == ''.join(sys.argv[1]):
+	#if "10day" == ''.join(sys.argv[1]):
 
 # TODO: Add ablilty to text myself in the morning to bring umbrella if it is going to rain
