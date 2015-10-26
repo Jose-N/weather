75672478c14b12abff83f5b30aec3cb9fc69b181
Jose-N <jose.c.naylor@gmail.com>
2015-10-26 09:09:23 -0400

    Added ability to get hourly temp and weather

--- weather.py
+++ weather.py
@@ -2,7 +2,7 @@
 # weather.py
 # Scrapes http://www.accuweather.com/ to grab weather information that can be called from command line
 
-import sys,requests
+import sys,requests, datetime
 from bs4 import BeautifulSoup
 
 # Activate from command line
@@ -29,7 +29,23 @@ if len(sys.argv) > 1:
 		print("The high today is " + todayHiTemp + "F" + " the low today is " + todayLowTemp + "F")
 		print( "Today is going to be " + todayWeather + " with " + tonightWeather + " at night")
 	# Get hourly weather
-	#if "hourly" == ''.join(sys.argv[1]):
+	if "hourly" == ''.join(sys.argv[1]):
+		date = datetime.datetime.now()
+		hour = date.strftime('%H')
+		for i in range(int(hour), 24):
+			hourlyRes = requests.get("http://www.accuweather.com/en/us/philadelphia-pa/19107/hourly-weather-forecast/350540?hour=" + str(i))
+			hourlyRes.raise_for_status()
+			hourlySoup = BeautifulSoup(hourlyRes.text, "html.parser")
+			hourlyTime = hourlySoup.select(".data .first-col")[0].getText()
+			if i < 19:
+				hourlyTemp = hourlySoup.select(".data .temp .bg-su")[0].getText()
+			else:
+				hourlyTemp = hourlySoup.select(".data .temp .bg-cl")[0].getText()
+			if i < 19:
+				hourlyForecast = hourlySoup.select(".data .forecast .icon")[0].getText()
+			else:
+				hourlyForecast = hourlySoup.select(".data .forecast .icon")[0].getText()
+			print("At " + hourlyTime + " it will be " + hourlyTemp + "F" + " and " + hourlyForecast)
 	# 10 day weather forecast
 	#if "10day" == ''.join(sys.argv[1]):
 
