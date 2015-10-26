b4b10d06b1db8dac89f74fed2277d1894573e648
Jose-N <jose.c.naylor@gmail.com>
2015-10-26 05:45:29 -0400

    Add logic to determine what to scrap off of website

--- weather.py
+++ weather.py
@@ -6,9 +6,13 @@ import sys,request
 for bs4 import BeautifulSoup
 
 # Activate from command line
-# Use if statments to determine what to do
+if len(sys.argv) > 1:
+	# Use if statments to determine what to do
 	# Get current weather
+	if "today" == ''.join(sys.argv[1]):
 	# Get hourly weather
+	if "hourly" == ''.join(sys.argv[1]):
 	# 10 day weather forecast
+	if "10day" == ''.join(sys.argv[1]):
 
 # TODO: Add ablilty to text myself in the morning to bring umbrella if it is going to rain
