50db630c78399a8f5e41a9cef47a717c53016c60
Jose-N <jose.c.naylor@gmail.com>
2015-10-26 05:36:31 -0400

    Add shebange! line, import statments, and used comments to outline program

---
+++ weather.py
@@ -0,0 +1,14 @@
+#! python3
+# weather.py
+# Scrapes http://www.wunderground.com/ to grab weather information that can be called from command line
+
+import sys,request
+for bs4 import BeautifulSoup
+
+# Activate from command line
+# Use if statments to determine what to do
+	# Get current weather
+	# Get hourly weather
+	# 10 day weather forecast
+
+# TODO: Add ablilty to text myself in the morning to bring umbrella if it is going to rain
